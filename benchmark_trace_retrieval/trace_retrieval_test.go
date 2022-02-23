0a72c778de59b0310cc957d1528b1d6cf8c0b170
Jessica Berg <jesswberg@gmail.com>
2022-02-23 17:34:20 -0500

    extra parallel tests

--- benchmark_trace_retrieval/trace_retrieval_test.go
+++ benchmark_trace_retrieval/trace_retrieval_test.go
@@ -26,14 +26,35 @@ func benchmarkTraceRetrieval(traceID string, b *testing.B) {
     result = s
 }
 
+func benchmarkTraceRetrievalParallel(traceID string, b *testing.B) {
+    var s string
+    ctx := context.Background()
+    client, err := storage.NewClient(ctx)
+    if err != nil {
+            log.Fatalf("Failed to create client: %v", err)
+    }
+    defer client.Close()
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        s, _ = getTraceParallelized(traceID, client)
+    }
+    result = s
+}
+
 func BenchmarkBigTrace(b *testing.B) {
     benchmarkTraceRetrieval("82b29a11332a18878a7d5664b583d983", b)
+}
+func BenchmarkBigTraceParallel(b *testing.B) {
+    benchmarkTraceRetrievalParallel("82b29a11332a18878a7d5664b583d983", b)
 }
 
 func BenchmarkFourSpanTrace(b *testing.B) {
     benchmarkTraceRetrieval("52b22dcfef3d16ac0bf6634f9ba61d5f", b)
 }
 
+func BenchmarkFourSpanTraceParallel(b *testing.B) {
+    benchmarkTraceRetrievalParallel("52b22dcfef3d16ac0bf6634f9ba61d5f", b)
+}
 func benchmarkFileRetrieval(filename string, b *testing.B) {
     var s []byte
     ctx := context.Background()
