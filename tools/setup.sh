39d7a7856b061d2b244a0c03b33a286ee49b35cf
fruffy <fruffy@nyu.edu>
2022-03-08 12:49:46 -0500

    Pin Go.

--- tools/setup.sh
+++ tools/setup.sh
@@ -25,7 +25,7 @@ minikube config set driver hyperkit
 # Try to install docker
 brew install  --cask docker docker-machine
 # Install go to build the telemetry collector
-brew install golang
+brew install golang@1.17.4
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 # Docker
 sudo apt install -y docker.io
@@ -44,7 +44,13 @@ rm minikube_latest_amd64.deb
 # Need to use docker because we are in a VM
 minikube config set driver docker
 # Install go to build the telemetry collector
-sudo apt install golang
+wget https://dl.google.com/go/go1.17.4.linux-amd64.tar.gz
+sudo tar -xvf go1.17.4.linux-amd64.tar.gz
+sudo mv go /usr/local
+echo 'export GOROOT=/usr/local/go ' >>~/.bash_profile
+# TODO(Fabian): Make this user-defined? I hate Go...
+echo 'export GOPATH=$HOME/GoProjects' >>~/.bash_profile
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >>~/.bash_profile
 fi
 
 # Configure minikube
