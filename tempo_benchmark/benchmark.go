654324b1d9fdbfa05793fd99e5c7bcabf12205ea
HaseebLUMS <mh6218@nyu.edu>
2022-03-09 10:20:18 -0500

    minor changes

--- tempo_benchmark/benchmark.go
+++ tempo_benchmark/benchmark.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-    "cloud.google.com/go/storage"
-    "context"
+	"context"
 	"fmt"
 	"io"
-    "io/ioutil"
-    "log"
-    "time"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
+
+	"cloud.google.com/go/storage"
 )
 
-const TempoQueryAddr = "http://34.72.20.79:16686/"
+const TempoQueryAddr = "http://34.121.49.198:16686/"
 const GetTraceEndpoint = "api/traces/"
 
 func getTrace(traceId string) (error, []byte) {
@@ -27,29 +28,33 @@ func getTrace(traceId string) (error, []byte) {
 		log.Fatalln(err)
 		return err, nil
 	}
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", resp.StatusCode, body)
+	}
 	return nil, body
 }
 
-func downloadFileIntoMemory(w io.Writer, bucket, object string, client* storage.Client) ([]byte, error) {
+func downloadFileIntoMemory(w io.Writer, bucket, object string, client *storage.Client) ([]byte, error) {
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
 	defer cancel()
 
 	rc, err := client.Bucket(bucket).Object(object).NewReader(ctx)
 	if err != nil {
-			return nil, fmt.Errorf("Object(%q).NewReader: %v", object, err)
+		return nil, fmt.Errorf("Object(%q).NewReader: %v", object, err)
 	}
 	defer rc.Close()
 
 	data, err := ioutil.ReadAll(rc)
 	if err != nil {
-			return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
+		return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
 	}
 	return data, nil
 }
 
 func main() {
-	err, trace := getTrace("6009004d2e8ca99b64a9a4e1924e4de3")
+	err, trace := getTrace("9767ed368bf2053d8ac8c360e799d3f2")
 	if err != nil {
 		log.Fatalln(err)
 	}
