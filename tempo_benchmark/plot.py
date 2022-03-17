bf3b5b002078468f63748c37609a4721215a9c4d
HaseebLUMS <mh6218@nyu.edu>
2022-03-17 16:26:20 -0400

    added bar plots

--- tempo_benchmark/plot.py
+++ tempo_benchmark/plot.py
@@ -6,7 +6,23 @@ import matplotlib.pyplot as plt
 
 fn = sys.argv[1]
 df = pd.read_csv(fn)
-df.plot(x="BenchName", y="Median", kind="bar", color="blue")
+
+fig, ax = plt.subplots()
+
+ax.barh(
+    df["BenchName"], 
+    df["Median"], 
+    color='blue', 
+    height=0.3,
+    xerr=df["CI"],
+    capsize=5
+)
+
+for i, v in enumerate(df["Median"]):
+    ax.text(v - 5, i + .25, "m: " + str(v),
+            color = 'red')
+    ax.text(v + 3, i + .25, "ci:" + str(df["CI"][i]))
+
 plt.tight_layout()
 plt.ylabel("Time (ms) ")
 plt.show()
