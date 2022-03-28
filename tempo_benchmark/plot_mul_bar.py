8e0c4fe8a4aaef01cec561258bd2d465dff41cbb
HaseebLUMS <mh6218@nyu.edu>
2022-03-28 11:40:05 -0400

    GCS reproducibility plots added

--- tempo_benchmark/plot_mul_bar.py
+++ tempo_benchmark/plot_mul_bar.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import sys
 import pandas as pd
 
-fn1 = "out-gcs-march-18-2.csv"
+fn1 = "out-gcs-march-23-run3.csv"
 df_cold = pd.read_csv(fn1)
 
-fn2 = "out-gcs-march-17-bvm-warm.csv"
+fn2 = "out-gcs-march-23-run2.csv"
+# fn2 = "out-gcs-march-22-run4.csv"
 df_warm = pd.read_csv(fn2)
 
 X = ['2 B', '100 B', '1,000B', '10,000 B', '100,000 B', 'MB', 'BigTrace(49kB)', 'SmallTrace(8kB)', 'TinyTrace(4kB)']
@@ -15,8 +16,8 @@ Warm = df_warm["Median"]
 
 X_axis = np.arange(len(X))
 
-plt.barh(X_axis - 0.2, Cold, 0.4, label = 'Cold')
-plt.barh(X_axis + 0.2, Warm, 0.4, label = 'Warm')
+plt.barh(X_axis - 0.2, Cold, 0.4, label = 'March 23 9:23 AM')
+plt.barh(X_axis + 0.2, Warm, 0.4, label = 'March 23 8:58 AM')
 
 for i, v in enumerate(df_warm["Median"]):
     plt.text(v - 5, i, str(v) + u"\u00B1" + str(df_warm["CI"][i]),
@@ -28,6 +29,6 @@ for i, v in enumerate(df_warm["Median"]):
 plt.yticks(X_axis, X)
 plt.xlabel("Median Time(ms)")
 plt.ylabel("Num of Bytes")
-plt.title("GCS Cold vs Warm")
+plt.title("GCS Repeated Warm Runs")
 plt.legend()
 plt.show()
