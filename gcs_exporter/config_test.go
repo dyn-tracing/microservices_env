262bee103eefeefa001fb198df526d862af4111a
Jessica Berg <jesswberg@gmail.com>
2023-01-17 18:30:08 -0500

    aaaahhhhhhh :(

--- gcs_exporter/config_test.go
+++ gcs_exporter/config_test.go
@@ -26,19 +26,15 @@ import (
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 )
 
+/*
 func TestLoadConfig(t *testing.T) {
     cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
 	require.NoError(t, err)
-	factory := NewFactory()
+
+    factory := NewFactory()
 	cfg := factory.CreateDefaultConfig()
 
     sub, err := cm.Sub(component.NewIDWithName(typeStr, "").String())
-	require.NoError(t, err)
-	require.NoError(t, component.UnmarshalConfig(sub, cfg))
-
-    assert.Equal(t, sanitize(cfg.(*Config)), sanitize(factory.CreateDefaultConfig().(*Config)))
-
-    sub, err = cm.Sub(component.NewIDWithName(typeStr, "customname").String())
 	require.NoError(t, err)
 	require.NoError(t, component.UnmarshalConfig(sub, cfg))
 
@@ -51,10 +47,71 @@ func TestLoadConfig(t *testing.T) {
     customConfig.NumOfDigitsForRandomHash = "2"
 	customConfig.Insecure = true
 	customConfig.TimeoutSettings = exporterhelper.TimeoutSettings{
-		Timeout: 20 * time.Second,
+		Timeout: 12 * time.Second,
 	}
 	customConfig.Compression = "gzip"
     assert.Equal(t, cfg, customConfig)
+}
+*/
+
+func TestLoadConfig2(t *testing.T) {
+	t.Parallel()
+
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, err)
+
+	// URL doesn't have a default value so set it directly.
+	defaultCfg := createDefaultConfig().(*Config)
+
+	tests := []struct {
+		id       component.ID
+		expected component.Config
+	}{
+		{
+			id:       component.NewIDWithName(typeStr, "customname"),
+			expected: defaultCfg,
+		},
+		{
+			id: component.NewIDWithName(typeStr, ""),
+			expected: &Config{
+				RetrySettings: exporterhelper.RetrySettings{
+					Enabled:         true,
+					InitialInterval: 10 * time.Second,
+					MaxInterval:     1 * time.Minute,
+					MaxElapsedTime:  10 * time.Minute,
+				},
+				QueueSettings: exporterhelper.QueueSettings{
+					Enabled:      true,
+					NumConsumers: 2,
+					QueueSize:    10,
+				},
+                ProjectID: "my-project",
+                UserAgent: "opentelemetry-collector-contrib {{version}}",
+                Endpoint: "test-endpoint",
+                BucketSuffix: "snicket4",
+                NumOfDigitsForRandomHash: "2",
+                Insecure: true,
+                TimeoutSettings: exporterhelper.TimeoutSettings{
+                    Timeout: 12 * time.Second,
+                },
+	            Compression: "gzip",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id.String(), func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, component.UnmarshalConfig(sub, cfg))
+
+			assert.NoError(t, component.ValidateConfig(cfg))
+			assert.Equal(t, tt.expected, cfg)
+		})
+	}
 }
 
 func TestCompressionConfigValidation(t *testing.T) {
